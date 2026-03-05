e2706cfe10ad35d9c87c8c1ea766e176badfdbba
Luis De Avila <bizangel10@gmail.com>
2026-03-05 22:10:25 +0000

    Launching example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,71 @@
+use std::io::{BufRead, BufReader};
+use std::process::{Command, Stdio};
+use std::thread;
+
+fn launch_game(runas: &str) {
+    unsafe {
+        std::env::set_var("SDL_VIDEODRIVER", "x11");
+    }
+
+    let args = vec![
+        "gamescope",
+        "-w",
+        "1920",
+        "-h",
+        "1080",
+        "--",
+        "bwrap",
+        "--die-with-parent",
+        "--dev-bind",
+        "/",
+        "/",
+        "steam",
+        "-console",
+        "steam://open/bigpicture",
+    ];
+
+    let mut child = Command::new("/home/arcanzu/scripts/gaming/run_as_user_gaming.sh")
+        .arg(runas)
+        .args(args)
+        .env("ENABLE_GAMESCOPE_WSI", "0")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .spawn()
+        .expect("failed to execute process");
+
+    let stdout = BufReader::new(child.stdout.take().unwrap());
+    let stderr = BufReader::new(child.stderr.take().unwrap());
+
+    let out_handle = thread::spawn(move || {
+        for line in stdout.lines() {
+            println!("{}", line.unwrap());
+        }
+    });
+    let err_handle = thread::spawn(move || {
+        for line in stderr.lines() {
+            eprintln!("{}", line.unwrap());
+        }
+    });
+
+    child.wait().unwrap();
+    out_handle.join().unwrap();
+    err_handle.join().unwrap();
+
+    println!("done")
+}
+
 fn main() {
-    println!("Hello, world!");
+    let game_users = vec!["game-user", "game-user-giluxe"];
+
+    let mut handles = Vec::new();
+
+    for user in game_users {
+        handles.push(thread::spawn(move || {
+            launch_game(user);
+        }));
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
