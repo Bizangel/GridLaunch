a329df686fb5cfec9c06a86343048666ae9cbf1e
Luis De Avila <bizangel10@gmail.com>
2026-03-06 00:33:39 +0000

    Working loop with struct refactors - using udev

--- src/main.rs
+++ src/main.rs
@@ -73,18 +73,45 @@
 // use std::path::PathBuf;
 // use udev::Enumerator;
 
+// fn main() {
+//     let vendor = "045e";
+//     let product = "0b00";
+//     let name = "Xbox Wireless Controller";
+
+//     match find_event_device(vendor, product, name) {
+//         Some(path) => println!("Matched device: {}", path.display()),
+//         None => println!("Device not found"),
+//     }
+// }
+
+use evdev::Device as EvdevDevice;
+// use udev::Moni
+use std::{collections::HashMap, path::PathBuf, thread, time::Duration};
+use udev::{Device, Enumerator, EventType, MonitorBuilder};
+
+pub fn is_joystick(event: &Device) -> bool {
+    let Some(devnode) = event.devnode() else {
+        return false;
+    };
+
+    if !devnode.to_string_lossy().contains("event") {
+        return false;
+    }
+
+    let is_joystick = event
+        .property_value("ID_INPUT_JOYSTICK")
+        .map(|val| val.to_string_lossy().to_string())
+        == Some("1".to_string());
+
+    return is_joystick;
+}
+
 // pub fn scan_devices(vendor_id: &str, product_id: &str, name: &str) -> Option<PathBuf> {
 //     let mut enumerator = Enumerator::new().ok()?;
-
-//     // Only search input devices
 //     enumerator.match_subsystem("input").ok()?;
 
 //     for device in enumerator.scan_devices().ok()? {
-//         // We only want event devices
-//         let Some(devnode) = device.devnode() else {
-//             continue;
-//         };
-//         if !devnode.to_string_lossy().contains("event") {
+//         if !is_joystick(&device) {
 //             continue;
 //         }
 
@@ -108,20 +135,6 @@
 //     None
 // }
 
-// fn main() {
-//     let vendor = "045e";
-//     let product = "0b00";
-//     let name = "Xbox Wireless Controller";
-
-//     match find_event_device(vendor, product, name) {
-//         Some(path) => println!("Matched device: {}", path.display()),
-//         None => println!("Device not found"),
-//     }
-// }
-
-use std::{thread, time::Duration};
-use udev::{EventType, MonitorBuilder};
-
 /// Returns the first non-empty NAME property from the device or any of its parents
 fn get_device_name(mut event: udev::Device) -> Option<String> {
     loop {
@@ -139,55 +152,105 @@ fn get_device_name(mut event: udev::Device) -> Option<String> {
     }
 }
 
-fn main() -> std::io::Result<()> {
-    // Build a monitor for input devices
-    let monitor = MonitorBuilder::new()
-        .unwrap()
-        .match_subsystem("input")
-        .unwrap()
-        .listen()
-        .unwrap();
+fn get_device_name_with_unk_default(dev: &Device) -> String {
+    get_device_name(dev.clone()).unwrap_or_else(|| "Unknown".into())
+}
 
-    println!("Waiting for devices...");
+use udev::MonitorSocket;
 
-    // Iterate over events
-    loop {
-        let event = monitor.iter().next();
+#[derive(Debug)]
+pub struct AppGamepad {
+    name: String,
+    devnode: PathBuf,
+    evdev_device: EvdevDevice,
+}
 
-        let Some(event) = event else {
-            thread::sleep(Duration::from_millis(100));
-            continue;
-        };
+pub struct GridLaunch {
+    devices: HashMap<PathBuf, AppGamepad>,
+    udev_monitor: MonitorSocket,
+}
 
-        let Some(devnode) = event.devnode() else {
-            continue;
-        };
+impl GridLaunch {
+    fn new() -> Result<GridLaunch, String> {
+        let monitor: MonitorSocket = MonitorBuilder::new()
+            .unwrap()
+            .match_subsystem("input")
+            .unwrap()
+            .listen()
+            .map_err(|err| err.to_string())?;
 
-        if !devnode.to_string_lossy().contains("event") {
-            continue;
-        }
+        Ok(GridLaunch {
+            devices: HashMap::new(),
+            udev_monitor: monitor,
+        })
+    }
 
-        let is_joystick = event
-            .property_value("ID_INPUT_JOYSTICK")
-            .map(|val| val.to_string_lossy().to_string())
-            == Some("1".to_string());
+    fn handle_udev_input_monitor_events(&mut self) {
+        for event in self.udev_monitor.iter() {
+            if !is_joystick(&event) {
+                continue;
+            }
 
-        if !is_joystick {
-            continue;
-        }
+            let Some(devnode) = event.devnode() else {
+                continue;
+            };
 
-        println!(
-            "Action: {:?}, Devnode: {:?}, Name: {:?}",
-            event.event_type(),
-            event.devnode(),
-            get_device_name(event.clone()) // event.property_value("NAME")
-        );
-
-        match event.event_type() {
-            EventType::Add => println!("Device added!"),
-            EventType::Remove => println!("Device removed!"),
-            EventType::Change => println!("Device changed!"),
-            _ => {}
+            match event.event_type() {
+                EventType::Add => {
+                    if self.devices.contains_key(devnode) {
+                        continue;
+                    }
+                    match EvdevDevice::open(&devnode) {
+                        Ok(dev) => {
+                            let gamepad = AppGamepad {
+                                devnode: devnode.to_path_buf(),
+                                name: get_device_name_with_unk_default(&event),
+                                evdev_device: dev,
+                            };
+                            let gamepadname = gamepad.name.clone();
+                            self.devices.insert(devnode.to_path_buf(), gamepad);
+                            println!("Added controller: {}", gamepadname);
+                        }
+                        Err(e) => eprintln!("Failed to open {:?}: {}", devnode, e),
+                    }
+                }
+                EventType::Remove => match self.devices.remove(devnode) {
+                    Some(dev) => println!("Removed controller: {:#?}", dev.name),
+                    None => {}
+                },
+                _ => {}
+            }
         }
     }
+
+    fn main_poll(&mut self) {
+        loop {
+            self.handle_udev_input_monitor_events();
+            thread::sleep(Duration::from_millis(100));
+        }
+    }
+}
+
+fn main() -> Result<(), String> {
+    let mut app = GridLaunch::new()?;
+
+    app.main_poll();
+    Ok(())
+
+    // Map of devnode -> (evdev device, name)
+    // let mut devices: HashMap<PathBuf, (EvdevDevice, String)> = HashMap::new();
+
+    // Iterate over events
+
+    // // Poll input events
+    // for (devnode, (device, name)) in devices.iter_mut() {
+    //     let Ok(events) = device.fetch_events() else {
+    //         continue;
+    //     };
+
+    //     for ev in events {
+    //         let evsumm = ev.destructure();
+    //         println!("Name: {:#?} on {}{:#?}", devnode, name, evsumm);
+    //     }
+    // }
 }
