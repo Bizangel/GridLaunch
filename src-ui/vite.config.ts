4343d424a2fa394d6f4620db0d8d5b98a4552bfc
Luis De Avila <bizangel10@gmail.com>
2026-03-07 22:28:49 +0000

    Added vite + frontend for UI tooling. boilerplate

---
+++ src-ui/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { viteSingleFile } from "vite-plugin-singlefile"
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react(), viteSingleFile()],
+  base: "./",
+  build: {
+    assetsInlineLimit: 0,
+    cssCodeSplit: false,
+    modulePreload: false
+  }
+})
